b5d291e706cfc6e7653c4323b1c5a86b50e3c645
Luisa Perez <luisa.perezw3@gmail.com>
2025-02-23 17:04:23 +0000

    return_the_day kata

---
+++ return_the_day.rb
@@ -0,0 +1,24 @@
+def day_of_the_week(n)
+  days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+  if n >= 8 || n <= 0
+    "Wrong, please enter a number between 1 and 7"
+  else
+    n = n - 1
+    days_of_the_week[n]
+  end
+
+end
+
+puts day_of_the_week(2)
+
+
+# Complete the function which returns the weekday according to the input number:
+
+# 1 returns "Sunday"
+# 2 returns "Monday"
+# 3 returns "Tuesday"
+# 4 returns "Wednesday"
+# 5 returns "Thursday"
+# 6 returns "Friday"
+# 7 returns "Saturday"
+# Otherwise returns "Wrong, please enter a number between 1 and 7"
